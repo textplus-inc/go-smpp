5d3734dad6ae0b68b1fb6c165bd21d82aeea34c2
Adrian Lopez <adrian@textplus.com>
2019-09-18 21:32:25 +0100

    append (N/T) at the end of long SMS part

--- smpp/receiver.go
+++ smpp/receiver.go
@@ -8,6 +8,7 @@ import (
 	"bytes"
 	"crypto/tls"
 	"fmt"
+	"strconv"
 	"sync"
 	"time"
 
@@ -17,13 +18,14 @@ import (
 
 // Receiver implements an SMPP client receiver.
 type Receiver struct {
-	Addr                 string
-	User                 string
-	Passwd               string
-	SystemType           string
-	EnquireLink          time.Duration
-	EnquireLinkTimeout   time.Duration // Time after last EnquireLink response when connection considered down
-	BindInterval         time.Duration // Binding retry interval
+	Addr               string
+	User               string
+	Passwd             string
+	SystemType         string
+	EnquireLink        time.Duration
+	EnquireLinkTimeout time.Duration // Time after last EnquireLink response when connection considered down
+	BindInterval       time.Duration // Binding retry interval
+	// NOTE: If MergeInterval > 0 for this textPlus branch, then append to the message the part number and total. ex: (1/3)
 	MergeInterval        time.Duration // Time in which Receiver waits for the parts of the long messages
 	MergeCleanupInterval time.Duration // How often to cleanup expired message parts
 	TLS                  *tls.Config
@@ -31,13 +33,6 @@ type Receiver struct {
 	SkipAutoRespondIDs   []pdu.ID
 
 	chanClose chan struct{}
-
-	// struct which holds the map of MergeHolders for the merging of the long incoming messages.
-	// It is used only if the incoming PDU holds UDH data and Receiver has MergeInterval > 0.
-	mg struct {
-		mergeHolders map[int]*MergeHolder
-		sync.Mutex
-	}
 
 	cl struct {
 		*client
@@ -48,14 +43,6 @@ type Receiver struct {
 // HandlerFunc is the handler function that a Receiver calls
 // when a new PDU arrives.
 type HandlerFunc func(p pdu.Body)
-
-// MergeHolder is a struct which holds the slice of MessageParts for the merging of a long incoming message.
-type MergeHolder struct {
-	MessageID     int
-	MessageParts  []*MessagePart // Slice with the parts of the message
-	PartsCount    int
-	LastWriteTime time.Time
-}
 
 // MessagePart is a struct which holds the data of the part of a long incoming message.
 type MessagePart struct {
@@ -92,16 +79,6 @@ func (r *Receiver) Bind() <-chan ConnStatus {
 	c.init()
 	go c.Bind()
 
-	// Set up message merging if requested
-	if r.MergeInterval > 0 {
-		if r.MergeCleanupInterval == 0 {
-			r.MergeCleanupInterval = 1 * time.Second
-		}
-
-		r.mg.mergeHolders = make(map[int]*MergeHolder)
-		go r.mergeCleaner()
-	}
-
 	return c.Status
 }
 
@@ -118,14 +95,6 @@ func (r *Receiver) bindFunc(c Conn) error {
 	if resp.Header().ID != pdu.BindReceiverRespID {
 		return fmt.Errorf("unexpected response for BindReceiver: %s",
 			resp.Header().ID)
-	}
-
-	// Clean the map in case of rebind, because message id numbering resets after reconnection
-	// and older IDs are no longer valid
-	if r.MergeInterval > 0 {
-		r.mg.Lock()
-		r.mg.mergeHolders = make(map[int]*MergeHolder)
-		r.mg.Unlock()
 	}
 
 	if r.Handler != nil {
@@ -146,16 +115,13 @@ func idInList(id pdu.ID, list []pdu.ID) bool {
 
 func (r *Receiver) handlePDU() {
 	var (
-		ok                bool
-		sm                *pdufield.SM
-		udhList           *pdufield.UDHList
-		msgID, partsCount int
-		mh                *MergeHolder
-		orderedBodies     []*bytes.Buffer
+		ok bool
+		// sm                  *pdufield.SM
+		udhList             *pdufield.UDHList
+		totalParts, partNum int
 	)
 	autoRespondDeliver := !idInList(pdu.DeliverSMID, r.SkipAutoRespondIDs)
 
-loop:
 	for {
 		p, err := r.cl.Read()
 		if err != nil || p == nil {
@@ -172,7 +138,7 @@ loop:
 			continue
 		}
 
-		sm, ok = p.Fields()[pdufield.ShortMessage].(*pdufield.SM)
+		_, ok = p.Fields()[pdufield.ShortMessage].(*pdufield.SM)
 		if !ok {
 			// PDU is malformed, do not process
 			continue
@@ -192,90 +158,19 @@ loop:
 					break
 				}
 
-				// Get message ID and total count of its parts
-				msgID = int(udh.IEData.Data[0])
-				partsCount = int(udh.IEData.Data[1])
+				// Get partNum and totalParts
+				//msgID := int(udh.IEData.Data[0])
+				totalParts = int(udh.IEData.Data[1])
+				partNum = int(udh.IEData.Data[2])
 
-				// Check if message part was already added to a MergeHolder
-				r.mg.Lock()
-				if mh, ok = r.mg.mergeHolders[msgID]; !ok {
-					mh = &MergeHolder{
-						MessageID:  msgID,
-						PartsCount: partsCount,
-					}
-
-					r.mg.mergeHolders[msgID] = mh
-				}
-				r.mg.Unlock()
-
-				// Add current part of the message to the slice
-				mh.MessageParts = append(mh.MessageParts, &MessagePart{
-					PartID: int(udh.IEData.Data[2]),
-					Data:   bytes.NewBuffer(sm.Data),
-				})
-				mh.LastWriteTime = time.Now()
-
-				fmt.Printf("OJOBUG: msgID-%v, partID-%v, partsCount-%v, mh.PartsCount-%v\n", udh.IEData.Data[0], mh.PartsCount, udh.IEData.Data[1], udh.IEData.Data[2])
-				fmt.Printf("OJOBUG: MergeHolder = %+v\n", mh)
-
-				if mh.PartsCount != partsCount {
-					fmt.Printf("Potentially wrong MsgID: %v\n", msgID)
-				}
-
-				// Check if we have all the parts of the message
-				if len(mh.MessageParts) != mh.PartsCount {
-					continue loop
-				}
-
-				// Order up PDUs
-				// FIXME: added +3 to partsCount due to panic - index out of range:
-				// panic: runtime error: index out of range [4] with length 3
-				// goroutine 110 [running]:
-				// github.com/fiorix/go-smpp/smpp.(*Receiver).handlePDU(0xc000386000)
-				// 	/go/pkg/mod/github.com/adrianlop/go-smpp@v0.0.0-20190912131229-0a1b0575a407/smpp/receiver.go:226 +0xa6f
-				// created by github.com/fiorix/go-smpp/smpp.(*Receiver).bindFunc
-				orderedBodies = make([]*bytes.Buffer, partsCount+3)
-				// TODO: maybe this is a FIX  ==> orderedBodies = make([]*bytes.Buffer, mh.PartsCount)
-				for _, mp := range mh.MessageParts {
-					orderedBodies[mp.PartID-1] = mp.Data
-				}
-
-				// Merge PDUs
-				var buf bytes.Buffer
-				for _, body := range orderedBodies {
-					if body != nil {
-						_, err := buf.Write(body.Bytes())
-						if err != nil {
-							continue
-						}
-					}
-				}
-
-				p.Fields().Set(pdufield.ShortMessage, buf.Bytes())
+				// appendMsgPart := "\n" + "(" + strconv.Itoa(partNum) + "/" + strconv.Itoa(totalParts) + ")"
+				// shortMsg := append(sm.Data, []byte(appendMsgPart)...)
+				p.Fields().Set("tp_longsms_part", []byte("("+strconv.Itoa(partNum)+"/"+strconv.Itoa(totalParts)+")"))
+				// p.Fields().Set(pdufield.ShortMessage, shortMsg)
 
 				// Handle
 				r.Handler(p)
 			}
-		}
-	}
-}
-
-func (r *Receiver) mergeCleaner() {
-	timer := time.NewTimer(r.MergeCleanupInterval)
-
-	for {
-		select {
-		case <-timer.C:
-			r.mg.Lock()
-			for _, mHolder := range r.mg.mergeHolders {
-				if time.Since(mHolder.LastWriteTime) > r.MergeInterval { // Message has expired, remove
-					delete(r.mg.mergeHolders, mHolder.MessageID)
-				}
-			}
-			r.mg.Unlock()
-
-		case <-r.chanClose:
-			return
 		}
 	}
 }
